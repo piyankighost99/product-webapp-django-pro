1c7bb345dfa97cb7cc4eed4d55c9de4c047a7487
Kalkidan Geletu <kalkidangeletu@gmail.com>
2024-03-01 19:18:07 +0300

    Delete shop directory

--- shop/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from shop.views.indexview import IndexView
-from shop.views.productdetailsview import ProductDetailsView
-
-urlpatterns = [
-    path("products", ProductDetailsView.as_view(), name="products"),
-    path("", IndexView.as_view(), name="index"),
-]
